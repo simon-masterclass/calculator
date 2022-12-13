453c8b7a31e4ceea5f696b8edef2d1c5f1289b03
Simon Ionashku <simon.ionashku@gmail.com>
2022-12-13 16:06:15 -0500

    [03] - Finished Calculator

--- script.js
+++ script.js
@@ -67,8 +67,23 @@ class Calculator {
 
     }
     
-    getDisplayNumber(number){
-        return number
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+
     }
 
     updateDisplay() {
@@ -78,12 +93,13 @@ class Calculator {
         if(this.previousOperand === undefined || this.previousOperand === null){
             this.previousOperand = ''
         } 
+       
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-        this.previousOperandTextElement.innerText = this.getDisplayNumber(this.previousOperand)
-
         if(this.operation != null) {
             this.previousOperandTextElement.innerText = 
-            `${this.previousOperand} ${this.operation}`
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 }
